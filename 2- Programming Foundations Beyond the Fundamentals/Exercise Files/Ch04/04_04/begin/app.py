675397c6d53860b8193b3c066aeec84377adf73e
Laouari Soheib <shepro19@gmail.com>
2021-09-02 12:25:33 +0100

    Challnge

--- 2- Programming Foundations Beyond the Fundamentals/Exercise Files/Ch04/04_04/begin/app.py
+++ 2- Programming Foundations Beyond the Fundamentals/Exercise Files/Ch04/04_04/begin/app.py
@@ -1,2 +1,9 @@
 miles = input('Enter a distance in miles: ')
 # kilometers_value = miles_value * 1.609344
+
+miles_float = float(miles)
+
+kilometers = miles_float * 1.609344
+
+print("This is the value converted from miles to kilometers")
+print(kilometers, 'Km')
